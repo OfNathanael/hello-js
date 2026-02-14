1742b6fd83b853324038c80af31c215a39e4c25f
Nathanael Shogbesan <nathanielshogbesan@gmail.com>
2026-02-14 18:29:57 +0100

    Practice

--- My Project/Practice.js
+++ My Project/Practice.js
@@ -2,3 +2,7 @@ const name = "Betechified";
 const date = "February 2026";
 console.log("I am a student of " + name + " " + date + " " + "cohort.");
 console.log("Javascript is actually interesting. My only issue now is remembering to add, commit, and push. But I know with time, I'll get the hang of it.");
+//git add . = This is used to add all the new changes made to my github repository.
+//git commit -m "[Name of the new commit]"  = This is used to commit the changes I have made to my github repository.
+//git push = This is used to push the changes I have made to my github repository.
+//git pull = This is used to pull the latest changes from my github repository to my local repository.
