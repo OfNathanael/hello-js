201a070adad110c72848a7d18825184c35df91dc
OfNathanael <nathanielshogbesan@gmail.com>
2026-02-14 18:14:21 +0100

    Practice

--- My Project/Practice.js
+++ My Project/Practice.js
@@ -1,3 +1,4 @@
 const name = "Betechified";
 const date = "February 2026";
 console.log("I am a student of " + name + " " + date + " " + "cohort.");
+console.log("Javascript is actually interesting. My only issue now is remembering to add, commit, and push. But I know with time, I'll get the hang of it.");
