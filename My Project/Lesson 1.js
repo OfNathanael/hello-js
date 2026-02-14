d91fc688c8c05515320519289d2239e853e9d536
Nathanael Shogbesan <nathanielshogbesan@gmail.com>
2026-02-14 18:03:11 +0100

    New file

---
+++ My Project/Lesson 1.js
@@ -0,0 +1,17 @@
+//const is used to store values that cannot be changed
+const name = "Nathanael";
+const age = 23;
+
+//let is used to store values that can be changed
+let city = "Abeokuta";
+
+//to output the values of the variables
+console.log(
+  "My name is " +
+    name +
+    " and I am " +
+    age +
+    " years old. I live in " +
+    city +
+    ".",
+);
