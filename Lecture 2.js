cc15e60a00614f53f02d7ac0cb65d2a236a9fa65
Nathanael Shogbesan <nathanielshogbesan@gmail.com>
2026-02-21 19:49:57 +0100

    Updated

---
+++ Lecture 2.js
@@ -0,0 +1,9 @@
+//NPM is a bult-in tool for installing, managing, and sharing packages of code. It is the default package manager for Node.js and is used to install and manage dependencies for JavaScript projects. With NPM, you can easily install packages from the NPM registry, which contains thousands of open-source packages that can be used in your projects. You can also use NPM to manage your project's dependencies, update packages, and publish your own packages to the registry.
+//Core commands are:
+//npm init - Initializes a new Node.js project and creates a package.json file.
+//npm install <package-name> - Installs a package and its dependencies.
+//npm update <package-name> - Updates a package to the latest version.
+//npm uninstall <package-name> - Uninstalls a package and removes it from the dependencies.
+//npm list - Lists all installed packages and their dependencies.
+//npm publish - Publishes a package to the NPM registry.
+//npm run <script-name> - Runs a script defined in the package.json file.
