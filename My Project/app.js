7326519e977ba47a80ac70367386c1c98565fb8f
Nathanael Shogbesan <nathanielshogbesan@gmail.com>
2026-02-21 20:34:00 +0100

    Updated

--- My Project/app.js
+++ My Project/app.js
@@ -1,3 +1,5 @@
+//import {add, subtract} from './math.js';
+//import can be used instead of const.
 const math = require('./math'); //Imports the math module, which contains the add and subtract functions defined in math.js.
 
 console.log(math.add(7, 15)); //Calls the add function from the math module with the arguments 3 and 15, and logs the result to the console.
--- My Project/math.js
+++ My Project/math.js
@@ -1,6 +1,7 @@
 //Modules
 
 //math.js exports a function that adds two numbers together. This function can be imported and used in other files to perform addition operations.
+//export function add(a, b) {
 function add(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
         throw new Error('Inputs must be numbers.'); //Error handling to ensure that the inputs are numbers. If not, an error is thrown with a descriptive message.
@@ -8,8 +9,10 @@ function add(a, b) {
     return a + b; //Returns the sum of a and b.
 }
 
+//export function subtract(a, b) {
 function subtract(a, b) {
     return a - b; //Returns the difference of a and b.
 }
 
+//This will not be used if we are using the import/export function.
 module.exports = { add, subtract }; //Exports the add and subtract functions as an object, allowing them to be imported and used in other files.
